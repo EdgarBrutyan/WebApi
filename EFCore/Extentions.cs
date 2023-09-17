4428dea413ae2fc0bc1634915d8b34b40f09928e
EdgarBrutyan <115035402+EdgarBrutyan@users.noreply.github.com>
2023-09-18 00:05:20 +0400

    Add files via upload

---
+++ EFCore/Extentions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore; // UseSqlite
+using Microsoft.Extensions.DependencyInjection; // IServiceCollection
+using System.Data.SqlTypes;
+namespace Packt.Shared;
+public static class NorthwindContextExtensions
+{
+ public static IServiceCollection AddNorthwindContext(
+ this IServiceCollection services, string relativePath = "..")
+    {
+        string databasePath = Path.Combine(relativePath, "Northwind.db");
+        services.AddDbContext<Nortwind>(options =>
+        options.UseSqlServer($"Data Source={databasePath}")
+        );
+        return services;
+    }
+}
---
+++ EFCore/Nortwind.cs
@@ -0,0 +1,78 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Packt.Shared
+{ 
+    public class Nortwind : DbContext
+    {
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet <Product>? Products { get; set; }
+
+
+        public Nortwind(DbContextOptions<Nortwind> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(
+        DbContextOptionsBuilder optionsBuilder)
+        {
+            string connection = "Data Source=.;" +
+            "Initial Catalog=Northwind;" +
+            "Integrated Security=true;" +
+            "MultipleActiveResultSets=true;";
+            optionsBuilder.UseSqlServer(connection);
+        } 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           modelBuilder.Entity<Product>().HasQueryFilter(e => e.ProductName.Contains("L"));
+        }
+    }
+
+
+    public class Category
+    {
+        Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        [Key]
+        public int CategoryId  { get; set; }
+
+        [Required]
+        public string? CategoryName { get; set; }
+
+        [Column (TypeName = "ntext")]
+        public string? Description { get; set; }
+
+      //  [InverseProperty(nameof(Product.Category))]
+        public virtual ICollection<Product> Products { get; set; }
+    }
+
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; } // первичный ключ
+
+        [Required]
+        [StringLength(40)]
+        public string ProductName { get; set; } = null!;
+
+        [Column("UnitPrice", TypeName = "money")]
+        public decimal? Cost { get; set; } // имя свойства != имя столбца
+   
+        [Column("UnitsInStock")]
+        public short? Stock { get; set; }
+
+        public bool Discontinued { get; set; }
+        // эти два параметра определяют отношение внешнего ключа
+        // к таблице Categories
+        public int CategoryId { get; set; }
+
+      //  public virtual Category Category { get; set; } = null!;
+    }
+
+}
